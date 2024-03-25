81666174b44354864ad01518ab0a962689a49fe2
matte <matteomanni1999@gmail.com>
2024-03-25 16:32:13 +0100

    seqsleepnet_cem

---
+++ physioex/train/networks/seqsleepnet_cem.py
@@ -0,0 +1,71 @@
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from pytorch_lightning.utilities.types import OptimizerLRScheduler
+
+from physioex.train.networks.base import SeqtoSeq
+from physioex.train.networks.seqsleepnet import EpochEncoder, SequenceEncoder
+
+module_config = dict()
+
+
+class SeqSleepNetCEM(SeqtoSeq):
+    def __init__(self, module_config=module_config):
+        super(SeqSleepNetCEM, self).__init__(
+            EpochEncoder(module_config), SequenceEncoderCEM(module_config), module_config
+        )
+
+class SequenceEncoderCEM(SequenceEncoder):
+    # TODO replace parameters hardcoded with module_config["cem_...."]
+    def __init__(self, module_config):
+        super(SequenceEncoderCEM, self).__init__()
+        self.n_concept = module_config["n_classes"] # dummy testing:= n_classes # TODO module_config["n_concept"] 
+        self.embedding_dim = 2 # TODO module_config["embedding_dim"]
+        self.latent_dim = module_config["latent_space_dim"]
+        self.concept_activations = []
+        
+        self.cem = CEM(self.latent_dim, self.n_concept, self.embedding_dim, self.concept_activations)
+
+        self.cls = nn.Linear(
+            self.n_concept * self.embedding_dim, module_config["n_classes"] 
+        )
+
+    def forward(self, x):
+        x = self.encode(x)
+        x = self.cem(x)
+        x = self.cls(x)
+        return x
+    
+class CEM(nn.Module):
+    def __init__(self, input_dim, n_concept, embedding_dim, concept_activations):
+        super(CEM, self).__init__()
+        self.concept_activations = concept_activations
+        
+        self.input2candidateConcepts = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(input_dim, embedding_dim),
+                nn.LeakyReLU(),)
+            for i in range(n_concept*2)]
+        )
+
+        self.score_function = nn.Sequential(
+            nn.Linear(embedding_dim*2, 1),
+            nn.Sigmoid(),
+        )
+        
+    def forword(self, x):
+        concepts = []
+        for i in range(self.n_concept):
+            c_plus = self.input2candidateConcepts[i](x)
+            c_minus = self.input2candidateConcepts[i+1](x)
+            c = torch.cat((c_plus, c_minus), 1) #volendo si potrebbe fare anche in verticale, quindi sulla dimensione 2 
+            score = self.score_function(c)
+            concept = score * c_plus + (1-score) * c_minus
+            concepts.append(concept)
+
+            self.concept_activations.append(score) #to take trace for the loss function about the concept activations
+
+        return torch.cat(concepts, 1)
+
