4bfa82d1ccba1c45b9630705fde55f641fa71af6
matteo manni <matteomanni1999@gmail.com>
2024-04-07 19:09:47 +0000

    binary_cem

--- physioex/train/networks/seqsleepnet_cem.py
+++ physioex/train/networks/seqsleepnet_cem.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from pytorch_lightning.utilities.types import OptimizerLRScheduler
+import torchmetrics as tm
 
 from physioex.train.networks.base import SeqtoSeq
 from physioex.train.networks.seqsleepnet import EpochEncoder, SequenceEncoder
@@ -18,6 +19,7 @@ class SeqSleepNetCEM(SeqtoSeq):
             SequenceEncoderCEM(module_config),
             module_config,
         )
+        self.class_division = eval(module_config["class_division"])
 
     def compute_loss(
         self,
@@ -33,12 +35,35 @@ class SeqSleepNetCEM(SeqtoSeq):
         # concepts[0] = concepts_embedding ; concepts[1] = concept_activations
         activations = concepts[1]
         activations = activations.reshape(batch_size * seq_len, -1)
-        outputs = outputs.reshape(-1, n_class)
+        
+        #TODO sistemare i target; attualmente funziona solo se i concetti e le classi sono uguali
         targets = targets.reshape(-1)
 
+        if n_class > 2:
+            outputs = outputs.reshape(-1, n_class)
+        else:
+            #Binary case
+            outputs = outputs.reshape(-1)
+            if(self.class_division != None):
+                binary_targets = torch.tensor([0 if t in self.class_division[0] else 1 for t in targets], dtype=torch.float32).to('cuda')
+                concepts_targets = targets.reshape(-1)
+                targets = (binary_targets, concepts_targets)
+            else:
+                print("class division is None") #TODO testare come entrare in questo caso cambiando il config
+
         # TODO fare l'assert della loss perché funziona solo con alcune loss
+        # TODO raffinare la logica di questa funzione
         loss = self.loss(activations, outputs, targets)
 
+        if n_class <= 2:
+            targets = targets[0]
+            self.acc = tm.Accuracy(task="binary").to("cuda")
+            self.f1 = tm.F1Score(task="binary").to("cuda")
+            self.ck = tm.CohenKappa(task="binary").to("cuda")
+            self.pr = tm.Precision(task="binary").to("cuda")
+            self.rc = tm.Recall(task="binary").to("cuda")
+        
+        #TODO questa parte potrebbe essere sposata in un'altra funzione (indipendente dal calcolo della loss) 
         self.log(f"{log}_loss", loss, prog_bar=True)
         self.log(f"{log}_acc", self.acc(outputs, targets), prog_bar=True)
         self.log(f"{log}_f1", self.f1(outputs, targets), prog_bar=True)
@@ -54,15 +79,21 @@ class SeqSleepNetCEM(SeqtoSeq):
 class SequenceEncoderCEM(SequenceEncoder):
     def __init__(self, module_config):
         super(SequenceEncoderCEM, self).__init__(module_config)
+        self.n_classes = module_config["n_classes"]
         self.n_concept = module_config["n_concepts"]
         self.concept_dim = module_config["concept_dim"]
         self.latent_dim = module_config["latent_space_dim"]
 
         self.cem = CEM(self.latent_dim, self.n_concept, self.concept_dim)
 
-        self.cls = nn.Linear(
-            self.n_concept * self.concept_dim, module_config["n_classes"]
-        )
+        if self.n_classes > 2:
+            self.cls = nn.Linear(self.n_concept * self.concept_dim, self.n_classes)
+        else:
+            # binary case
+            self.cls = nn.Sequential(
+                nn.Linear(self.n_concept * self.concept_dim, 1),
+                nn.Sigmoid()
+            )
 
     def forward(self, x):
         x, _ = self.encode(x)
--- physioex/train/networks/utils/loss.py
+++ physioex/train/networks/utils/loss.py
@@ -48,22 +48,57 @@ class CrossEntropyLoss(nn.Module, PhysioExLoss):
 
 
 class CrossEntropyLossCEM(nn.Module, PhysioExLoss):
-    # TODO extend concepts to non dummy concepts
+    # TODO implement the case in which concepts are not equal to classes
     def __init__(self, params: Dict = None):
         super(CrossEntropyLossCEM, self).__init__()
 
         # check if class weights are provided in params
-        if params is not None:
-            weights = params.get("class_weights", None)
-        else:
-            weights = None
+        weights = params.get("class_weights") if params is not None else None
         self.ce_loss_target = nn.CrossEntropyLoss(weight=weights)
         self.ce_loss_concepts = nn.CrossEntropyLoss()
 
-    def forward(self, emb, preds, targets):
+    def forward(self, activations, preds, targets):
         loss_target = self.ce_loss_target(preds, targets)
-        # TODO substitute targets with target_comcepts
-        loss_concepts = self.ce_loss_concepts(emb, targets)
+        loss_concepts = self.ce_loss_concepts(activations, targets)
+        return loss_target + loss_concepts
+
+
+class BCELossCEM(nn.Module, PhysioExLoss):
+    def __init__(self, params: Dict = None):
+        super(BCELossCEM, self).__init__()
+        self.class_division = ((0,), (1, 2, 3, 4))
+        # check if class weights are provided in params and in case compute the weights for the two classes
+        if params is not None:
+            self.weights = torch.zeros(2, requires_grad=False)
+            base_weights = params.get("class_weights")
+            if self.class_division is None:
+                self.weights = base_weights
+            else:
+                base_weights = 1/base_weights
+                for i in self.class_division[0]:
+                    self.weights[0] += base_weights[i]
+                for i in self.class_division[1]:
+                    self.weights[1] += base_weights[i]
+                self.weights = 1/self.weights
+                self.weights = self.weights / self.weights.sum()
+        else:
+            self.weights = None
+
+        self.ce_loss_concepts = nn.CrossEntropyLoss()
+
+    def forward(self, activations, preds, targets):
+        binary_targets = targets[0]
+        concept_targets = targets[1]
+    
+        if (self.weights is not None):
+            weights = torch.where(binary_targets == 0, self.weights[0], self.weights[1]).to('cuda')
+        else:
+            weights = None
+
+        bce_loss = nn.BCELoss(weight=weights)
+        loss_target = bce_loss(preds, binary_targets)
+        loss_concepts = self.ce_loss_concepts(activations, concept_targets)
+
         return loss_target + loss_concepts
 
 
@@ -71,4 +106,5 @@ config = {
     "cel": CrossEntropyLoss,
     "scl": SimilarityCombinedLoss,
     "cel_cem": CrossEntropyLossCEM,
+    "bcel_cem": BCELossCEM,
 }
